4c78ec36b6d4eb58d6cb564388c677d34319096f
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-08-04 19:51:19 +0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	s := r.PathPrefix("/v1").Subrouter()
+
+	s.HandleFunc("/register", nil).Methods("POST")
+	s.HandleFunc("/login", nil).Methods("POST")
+	s.HandleFunc("/logout", nil).Methods("POST")
+	s.HandleFunc("/refresh", nil).Methods("POST")
+	s.HandleFunc("/validate", nil).Methods("POST")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down")
+	server.Shutdown(ctx)
+	os.Exit(0)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module securebit
+
+go 1.23.1
+
+require github.com/gorilla/mux v1.8.1
