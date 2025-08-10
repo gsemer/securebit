3953e2ecdf105f659eb5a6a0f8c707daa1df013b
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-08-10 16:07:26 +0300

    separate payload, register and login request, and user in db

--- domain/users.go
+++ domain/users.go
@@ -1,8 +1,25 @@
 package domain
 
-type User struct {
+import "github.com/google/uuid"
+
+type UserPayload struct {
 	Username string `json:"username"`
-	Password string `json:"password"`
 	Role     string `json:"role"`
 	Email    string `json:"email"`
+}
+
+type RegisterRequest struct {
+	Password string `json:"password"`
+	*UserPayload
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AuthUser struct {
+	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	Username       string    `gorm:"type:text;not null;unique" json:"username"`
+	HashedPassword string    `gorm:"type:text;not null" json:"-"`
 }
