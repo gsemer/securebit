89edc2740571cd3a232234a421e88a0dda0ac472
Γεώργιος Σεμερτζάκης <gsemertzakis@hotmail.com>
2025-08-10 19:04:35 +0300

    move request user to presentation

--- domain/users.go
+++ domain/users.go
@@ -8,11 +8,6 @@ type UserPayload struct {
 	Email    string `json:"email"`
 }
 
-type RegisterRequest struct {
-	Password string `json:"password"`
-	*UserPayload
-}
-
 type LoginRequest struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -22,4 +17,8 @@ type AuthUser struct {
 	ID             uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
 	Username       string    `gorm:"type:text;not null;unique" json:"username"`
 	HashedPassword string    `gorm:"type:text;not null" json:"-"`
+}
+
+type UserRepository interface {
+	Create(authUser AuthUser) error
 }
